7ce65f4f37787df98744da7b3b079aa6dadae51a
ala saihi <alawork001@gmail.com>
2024-12-19 23:06:30 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 //------App Config-----
 const app = express()
@@ -14,7 +15,11 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
+
 //------APIEndPoints---
+app.use('/api/admin', adminRouter)
+//localhost:4000/api/admin/add-doctor
+
 app.get('/',(req,res) => {
   res.send("API Working")
 }
