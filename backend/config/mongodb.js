956192a20cde985dff4e91f951f644a431a3aae5
ala saihi <alawork001@gmail.com>
2024-12-23 23:14:00 +0100

    modifications

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose"
 
 const connectDB = async() => {
-    mongoose.connection.on('Connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/meditime`)
+    try {
+        mongoose.connection.on('connected', () => console.log("Database Connected"))
+        await mongoose.connect(process.env.MONGODB_URI);
+    } catch (error) {
+        console.error("Database Connection, Error:", error.message)
+    }
 }
 
 export default connectDB
